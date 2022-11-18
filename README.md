79c4bffd5d372b262061b484b10aa4483da10261
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2022-11-18 19:48:47 -0300

    Criação da pasta php
    na pasta contem os arquivos, index, painel, usuario-invalido e validar

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# login
+# Aula 02
+ - Envio de informações para o PHP
+ - Operadores relacionais
+ - Operadores lógicos
+ - Estrutura de decisão
+ - Redirecionamento de página
+
  
---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form>
+        Usuário:<input><br>
+        Senha:<input><br>
+        <button>Entra</button>
+    </form>
+
+    <footer>
+        <p>Todos os direitos reservados &copy; <?php echo date("Y");?></p>
+    </footer>
+</body>
+</html>
